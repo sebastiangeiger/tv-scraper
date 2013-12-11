8171b142e65fa72fb47281315933bcb04df13391
Sebastian Geiger <sebastian.geiger@gmail.com>
2013-12-11 16:21:19 +0100

    Creating episode lists works

--- test/tv_scraper/core_test.clj
+++ test/tv_scraper/core_test.clj
@@ -1,2 +1,41 @@
-(ns tv-scraper.core-test)
+(ns tv-scraper.core-test
+  (:require [clojure.test :refer :all]
+            [tv-scraper.core :refer :all]
+            [tv-scraper.imdb]
+            [tv-scraper.epguides]))
+(deftest ^:wip producing-episode-lists
+         (let [show {:seasons {:1 {:episodes {:1 {:title "S01E01"}}}
+                               :2 {:episodes {:1 {:title "S02E01"}}}}}]
+           (is (= (episode-list show)
+                  #{{:seasons :1 :episodes :1 :title "S01E01"}
+                    {:seasons :2 :episodes :1 :title "S02E01"}})))
+         )
 
+(deftest ^:wip testing-flat?
+         (is (flat? {:hello 'world}))
+         (is (flat? [{:hello 'world}{:and 'something}]))
+         (is (not (flat? {:hello {:beautiful 'world} :and 'something})))
+         )
+
+(deftest ^:wip testing-paths-through
+         (let [show {:seasons {:1 {:episodes {:1 {:title "S01E01"}}}
+                               :2 {:episodes {:1 {:title "S02E01"}
+                                              :2 {:title "S02E02"}}}}}
+               simple {:k1 {:inner :map} :k2 {:something :else}}
+               two {:k1 {:k11 {:inner :map} :k12 {:something :else}}
+                    :k2 {:k21 {:inner :map} :k22 {:something :else} :k23 {:and :this}}
+                    :k3 {:k31 {:only :this}}}]
+           (is (= (paths-through {:inner :map}) #{}))
+           (is (= (paths-through simple) #{[:k1][:k2]}))
+           (is (= (paths-through two) #{[:k1 :k11][:k1 :k12]
+                                        [:k2 :k21][:k2 :k22][:k2 :k23]
+                                        [:k3 :k31]}))
+           (is (= (paths-through show) #{[:seasons :1 :episodes :1]
+                                         [:seasons :2 :episodes :1]
+                                         [:seasons :2 :episodes :2]}))))
+
+(deftest ^:wip checking-if-they-produce-the-same-results
+         (is (= (-> "http://www.imdb.com/title/tt0805663" tv-scraper.imdb/parse-show-page episode-list) []))
+         ;; (is (= (-> "http://www.imdb.com/title/tt0805663" tv-scraper.imdb/parse-show-page episode-list)
+         ;;        (-> "http://epguides.com/Jericho/"    tv-scraper.epguides/parse-show-page episode-list))))
+                )
